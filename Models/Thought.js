bd3dd32d3f094b710d4f48ba0d227348756c5ecf
Kyle Patrick Bader <kbader91@gmail.com>
2022-04-30 12:56:20 -0700

    reformatted thought/reaction file setup

--- Models/Thought.js
+++ Models/Thought.js
@@ -1,6 +1,34 @@
-const { Schema, model } = require('mongoose');
+const { Schema, Types, model } = require('mongoose');
 const dateFormat = require('../utils/dateFormat');
-const ReactionSchema = require('./Reactions');
+
+const ReactionsSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: new Types.ObjectId()
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            maxlength: 280,
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => dateFormat(createdAtVal)
+        }
+    },
+    {
+        toJSON: {
+            getters: true
+        }
+    }
+);
+
 
 const ThoughtSchema = new Schema(
     {
@@ -19,7 +47,7 @@ const ThoughtSchema = new Schema(
             type: String,
             required: true,
         },
-        reactions: [ReactionSchema]
+        reactions: [ReactionsSchema]
     },
     {
         toJSON: {
